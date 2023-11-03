c279adcae86739e920648e517d4dd34f6fc8b9d8
Intjar Mansuri <intjar1606@gmail.com>
2023-11-03 17:31:14 +0530

    Array

---
+++ Arrays/Two Pointer/283-Move_Zeroes.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void moveZeros(vector<int>& nums) {
+    int n = nums.size();
+
+    int i = 0;
+
+    for(int j = 0; j<n; j++) {
+        
+        if(nums[j] != 0) {
+            swap(nums[j], nums[i]);
+            i++;
+        }
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int>nums(n);
+    for(int i = 0; i<n; i++) {
+        cin >> nums[i];
+    }
+
+    moveZeros(nums);
+    for(int i = 0; i < n; i++) {
+        cout << nums[i] << " ";
+    }
+
+
+    return 0;
+}
