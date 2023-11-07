7a35e5f775d00386ca2f4f0a93c6134e6fe74d2b
Intjar Mansuri <intjar1606@gmail.com>
2023-11-07 21:13:24 +0530

    Simple Sliding Window

---
+++ Array/Sliding Window/1493-Longest_Subarray_of_1s_After_Deleting_One_Element_Ap-2.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int longestSubarray(vector<int>& nums) {
+    int zeroCount = 0;
+    int maxLength = 0;
+
+    int i = 0;
+    for(int j = 0; j<nums.size(); j++) {
+        if(nums[j] == 0) {
+            zeroCount++;
+        }
+
+        while(zeroCount > 1) {
+            if(nums[i] == 0) {
+                zeroCount--;
+            }
+            i++;
+        }
+        maxLength = max(maxLength, j-i);
+    }
+    return maxLength;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+    for(int i = 0; i<n; i++) {
+        cin >> nums[i];
+    }
+
+    int ans = longestSubarray(nums);
+    cout << ans << endl;
+
+    return 0;
+}
