bdf726a75db3c270c3769fa9239dda0be8d21d6f
Intjar Mansuri <intjar1606@gmail.com>
2023-12-24 20:00:51 +0530

    Approach - 1

---
+++ String/1758-Minimum_Changes_To_Make_Alternating_Binary_String_Ap-1.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int minOperations(string s) {
+    int n = s.length();
+
+    int start_with_zero = 0;
+    int start_with_one = 0;
+
+    for(int i = 0; i<n; i++) {
+        if(i % 2 == 0) {
+            if(s[i] == '0') {
+                start_with_one++;
+            } else {
+                start_with_zero++;
+            }
+        } else {
+            if(s[i] == '1') {
+                start_with_one++;
+            } else {
+                start_with_zero++;
+            }
+        }
+    }
+    return min(start_with_zero, start_with_one);
+}
+
+int main() {
+    string s;
+    cin >> s;
+
+    int result = minOperations(s);
+    cout << result << endl;
+
+    return 0;
+}
