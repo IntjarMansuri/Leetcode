6aa1b7782e149c590c4335024a1090601c3def3f
Intjar Mansuri <intjar1606@gmail.com>
2024-04-25 00:44:04 +0530

    Approach 1

---
+++ DP/1137-N-th_Tribonacci_Number_Ap-1.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    int dp[38];
+    int find(int n) {
+        if(n == 0) {
+            return 0;
+        }
+
+        if(n == 1 || n == 2) {
+            return 1;
+        }
+
+        if(dp[n] != -1) {
+            return dp[n];
+        }
+
+        int a = find(n-1);
+        int b = find(n-2);
+        int c = find(n-3);
+
+        return dp[n] =  a + b + c;
+    }
+    
+    int tribonacci(int n) {
+        memset(dp, -1, sizeof(dp));
+        return find(n);
+    }
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << tribonacci(n) << endl;
+
+    return 0;
+}
+
+// Approach : Recursion + Memoization
