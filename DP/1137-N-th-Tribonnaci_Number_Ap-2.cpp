511b67a527980da7796d3676400a11e481629810
Intjar Mansuri <intjar1606@gmail.com>
2024-04-25 01:00:54 +0530

    Approach 2

---
+++ DP/1137-N-th-Tribonnaci_Number_Ap-2.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int tribonacci(int n) {
+        int dp[38];
+
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for(int i = 3; i<=37; i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+
+        return dp[n];
+    }
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << tribonacci(n) << endl;
+    return 0;
+}
